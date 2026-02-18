08a1840c373057126a65ba8ac0f5dd7f6e4f6da0
kerangleeya <shelia.vellicita13@gmail.com>
2026-02-18 12:28:25 +0700

    add unit test for ProductService to increase coverage

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceImplTest {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository productRepository; // Used to verify data directly
+
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        service.create(product);
+
+        List<Product> productList = service.findAll();
+        assertFalse(productList.isEmpty());
+        Product savedProduct = productList.get(0);
+
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        assertEquals(product.getProductName(), savedProduct.getProductName());
+        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+    }
+}
